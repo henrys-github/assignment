649b0a54aad7ddfce0ef6218e19f8d4cde292dee
Henry <banana970@yahoo.com.tw>
2018-09-24 03:01:51 +0800

    modifying

--- src/plugin/speed_test_curl/speed_test_curl.c
+++ src/plugin/speed_test_curl/speed_test_curl.c
@@ -18,71 +18,71 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- ***************************************************************************/ 
+ ***************************************************************************/
 /* <DESC>
  * Download a given URL into a local file named page.out.
  * </DESC>
- */ 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
- 
+
 #include <curl/curl.h>
- 
+
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
   size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
   return written;
 }
- 
+
 int curl_download(char* url)
 {
   CURL *curl_handle;
   static const char *pagefilename = "page.out";
   FILE *pagefile;
- 
+
   if(url == NULL) {
     printf("url is NULL\n");
     return 1;
   }
- 
+
   printf("hanhan\n");
   curl_global_init(CURL_GLOBAL_ALL);
- 
-  /* init the curl session */ 
+
+  /* init the curl session */
   curl_handle = curl_easy_init();
- 
-  /* set URL to get here */ 
+
+  /* set URL to get here */
   curl_easy_setopt(curl_handle, CURLOPT_URL, url);
- 
-  /* Switch on full protocol/debug output while testing */ 
+
+  /* Switch on full protocol/debug output while testing */
   curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
- 
-  /* disable progress meter, set to 0L to enable and disable debug output */ 
+
+  /* disable progress meter, set to 0L to enable and disable debug output */
   curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
- 
-  /* send all data to this function  */ 
+
+  /* send all data to this function  */
   curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
- 
-  /* open the file */ 
+
+  /* open the file */
   pagefile = fopen(pagefilename, "wb");
   if(pagefile) {
- 
-    /* write the page body to this file handle */ 
+
+    /* write the page body to this file handle */
     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
- 
-    /* get it! */ 
+
+    /* get it! */
     curl_easy_perform(curl_handle);
- 
-    /* close the header file */ 
+
+    /* close the header file */
     fclose(pagefile);
   }
- 
-  /* cleanup curl stuff */ 
+
+  /* cleanup curl stuff */
   curl_easy_cleanup(curl_handle);
- 
+
   curl_global_cleanup();
- 
+
   return 0;
 }
 
